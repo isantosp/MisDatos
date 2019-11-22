2bb6fbcdd020450dc0c217d2b1b0af2751572242
Ivan Santos <untalivan@isantosp.com>
2019-11-21 18:14:48 -0600

    Initial commit

---
+++ app/src/main/java/net/ddsmedia/connect/misdatos/DatosActivity.java
@@ -0,0 +1,14 @@
+package net.ddsmedia.connect.misdatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
---
+++ app/src/main/java/net/ddsmedia/connect/misdatos/MainActivity.java
@@ -0,0 +1,54 @@
+package net.ddsmedia.connect.misdatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnDatos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnDatos = findViewById(R.id.btnDatos);
+
+        mBtnDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        DatosActivity.class);
+                startActivity(intent);
+
+
+                Toast.makeText(getApplicationContext(),
+                        "Hola soy el boton Datos",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void clickEnBotones(View view){
+        switch (view.getId()){
+            /*case R.id.btnDatos:
+                Toast.makeText(getApplicationContext(),
+                        "Hola soy el boton Datos desde fuera",
+                        Toast.LENGTH_LONG).show();
+                break;*/
+
+            case R.id.otroBoton:
+                Toast.makeText(getApplicationContext(),
+                        "Hola soy el otro boton",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+
+}
